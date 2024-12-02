f0f0f3bb2fd7b56a4e916fe03fca67e9b01a02bb
Sanalouafi <sanalouafi2003@gmail.com>
2024-12-02 11:57:36 +0100

    create eBankify-p1 project

---
+++ src/test/java/com/example/ebankifyp1/EBankifyP1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ebankifyp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankifyP1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
