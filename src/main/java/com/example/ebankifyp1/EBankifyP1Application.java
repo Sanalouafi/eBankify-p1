1ac1cc3b3968ca244276606e31b0c0856371cdf7
Sanalouafi <sanalouafi2003@gmail.com>
2024-12-02 11:57:36 +0100

    create eBankify-p1 project

---
+++ src/main/java/com/example/ebankifyp1/EBankifyP1Application.java
@@ -0,0 +1,13 @@
+package com.example.ebankifyp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBankifyP1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBankifyP1Application.class, args);
+    }
+
+}
